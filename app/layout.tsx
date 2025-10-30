cba1a9d05a526f393e87f48712f31e75e455742c
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-10-30 16:12:52 +0000

    add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { Archivo_Black, Space_Grotesk } from "next/font/google";
 import './globals.css';
 
@@ -21,9 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${archivoBlack.variable} ${space.variable}`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
