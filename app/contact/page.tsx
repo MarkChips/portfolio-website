f3232ae024cd9c5dac883aa2820d55d595c19abe
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-11-06 15:43:40 +0000

    create contact/page.tsx with <h1> text

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@/components/retroui/Text";
+
+export default function Page() {
+    return (
+        <main className="flex min-h-screen w-full max-w-4xl flex-col items-center justify-around py-32 px-16 bg-white dark:bg-black sm:items-start">
+            <Text as="h1">Contact Me</Text>
+        </main>
+    );
+}
